938a1e93cbe525ef005963998fd46b71886b6fe4
Jeimy Rodriguez <jeimyrodriguez@jeimys-mbp.home>
2020-04-18 20:16:16 -0700

    updates

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ document.addEventListener("keydown", changeSize);
 
 function changeSize() {
     var e = event.keyCode;
-    var bSize = document.getElementById("balloonImg").style.fontSize;
+    var size = document.getElementById("balloon").style.fontSize;
     if (e == 73){
         if (parseInt(bSize)>60){
-            document.getElementById("balloonImg").innerHTML = "💥";
+            document.getElementById("balloon").innerHTML = "💥";
             document.removeEventListener('keyup', changeSize);
         }
         else{
-            if(bSize==""){
-                bSize=30;
+            if(size==""){
+                size=30;
             }
-            var inflateSize = parseInt(bSize) + 5;
-            document.getElementById("balloonImg").style.fontSize=inflateSize.toString()+"px";
+            var inflateSize = parseInt(size) + 5;
+            document.getElementById("balloon").style.fontSize=inflateSize.toString()+"px";
         }
         
     }
     else if (e == 68){
         console.log("d was pressed");
-        if(parseInt(bSize)==0){
-            document.getElementById("balloonImg").innerHTML = "Done";
+        if(parseInt(size)==0){
+            document.getElementById("balloon").innerHTML = "Done";
             document.removeEventListener('keyup', changeSize);
         }
         else{
-            if(bSize==""){
-                bSize=30;
+            if(size==""){
+                size=30;
             }
-            var deflateSize = parseInt(bSize) - 10;
-            document.getElementById("balloonImg").style.fontSize = deflateSize.toString() +"px";
+            var deflateSize = parseInt(size) - 10;
+            document.getElementById("balloon").style.fontSize = deflateSize.toString() +"px";
         }        
     }
 }
